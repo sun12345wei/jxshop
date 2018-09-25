56aa1a61ba871633913054bd43e6f0109b3f4833
sunwei <sunwei@czxy.com>
2018-09-25 20:13:02 +0800

    '加载后台模板'

---
+++ controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+namespace controllers;
+
+class IndexController
+{
+    public function index()
+    {
+        view('index/index');
+    }
+
+    public function top()
+    {
+        view('index/top');
+    }
+
+    public function menu()
+    {
+        view('index/menu');
+    }
+
+    public function main()
+    {
+        view('index/main');
+    }
+}
