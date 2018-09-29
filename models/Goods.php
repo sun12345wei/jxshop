f0a4deb9822e7886aa81efb476983d1312be4c60
sunwei <sunwei@czxy.com>
2018-09-29 15:24:33 +0800

    '添加商品SKU和商品图片'

--- models/Goods.php
+++ models/Goods.php
@@ -44,7 +44,6 @@ class Goods extends Model
         // die;
 
         // 处理商品属性
-
         $stmt = $this->_db->prepare("INSERT INTO goods_attribute(attr_name,attr_value,goods_id) VALUES(?,?,?)");
         // 循环每一个属性，插入到属性表
         foreach($_POST['attr_name'] as $k => $v)
@@ -57,7 +56,43 @@ class Goods extends Model
         }
 
         // 商品图片
+        $uploader = \libs\Uploader::make();
+
+        $stmt = $this->_db->prepare("INSERT INTO goods_image(goods_id,path) VALUES(?,?)");
+        $_tmp = [];
+        foreach($_FILES['image']['name'] as $k => $v)
+        {
+            // 拼出每张图片需要的数组
+            $_tmp['name'] = $v;
+            $_tmp['type'] = $_FILES['image']['type'][$k];
+            $_tmp['tmp_name'] = $_FILES['image']['tmp_name'][$k];
+            $_tmp['error'] = $_FILES['image']['error'][$k];
+            $_tmp['size'] = $_FILES['image']['size'][$k];
+
+            // 放到 $_FILES 数组中
+            $_FILES['tmp'] = $_tmp;
+
+            $path = '/uploads/'.$uploader->upload('tmp', 'goods');
+
+            // 执行SQL
+            $stmt->execute([
+                $this->data['id'],
+                $path,
+            ]);
+        }
+
+        
 
         // SKU
-    }
+        $stmt = $this->_db->prepare("INSERT INTO goods_sku(goods_id,sku_name,stock,price) VALUES(?,?,?,?)");
+        foreach($_POST['sku_name'] as $k => $v)
+        {
+            $stmt->execute([
+                $this->data['id'],
+                $v,
+                $_POST['stock'][$k],
+                $_POST['price'][$k],
+            ]);
+        }
+    }   
 }
